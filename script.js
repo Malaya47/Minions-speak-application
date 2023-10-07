6343df934ed2187c6924df3e34435d44d089b149
Malaya Tiwari <sidzilegend@gmail.com>
2023-10-08 01:53:08 +0530

    function to handle url with a text

--- script.js
+++ script.js
@@ -7,3 +7,11 @@ const url = "https://api.funtranslations.com/translate/minion.json";
 const textArea = document.querySelector(".text-js");
 const buttonTranslate = document.querySelector(".btn-js");
 const translatedText = document.querySelector(".translated-text");
+
+// function to handle the url with the text
+
+function translated(innerText) {
+  let completeUrl = url + "?" + "text=" + innerText;
+  return completeUrl;
+  // console.log(completeUrl);
+}
