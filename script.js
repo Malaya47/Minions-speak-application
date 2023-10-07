c9445e584b9a50a0546848cc12958f3628f8db5b
Malaya Tiwari <sidzilegend@gmail.com>
2023-10-08 01:50:05 +0530

    attached api link and selecting dom elements

---
+++ script.js
@@ -0,0 +1,9 @@
+// API url
+
+const url = "https://api.funtranslations.com/translate/minion.json";
+
+// selecting the elements
+
+const textArea = document.querySelector(".text-js");
+const buttonTranslate = document.querySelector(".btn-js");
+const translatedText = document.querySelector(".translated-text");
